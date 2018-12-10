5955b98d73db36de6734cafea16e4740e34a939c
Joseph <78599297@qq.com>
2018-12-10 16:38:20 +0800

    init

---
+++ src/main/java/com/cafe/study/domain/App.java
@@ -0,0 +1,24 @@
+package com.cafe.study.domain;
+
+import java.awt.List;
+import java.util.Collections;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	 private static List<String> getUser(int l ) {
+	    	return Collections.EMPTY_LIST;
+		}
+    public static void main( String[] args )
+    {
+    	
+    	int l=1;
+    	List<String> list=getUser(l);
+        System.out.println( "Hello World!" );
+    }
+    
+   
+}
