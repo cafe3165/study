2cb6da769be769b6eb886cfa37a111dc2562a85b
cafe3165 <78599297@qq.com>
2018-12-11 11:28:31 +0800

    some reflect test

--- src/main/java/com/cafe/study/domain/Apple.java
+++ src/main/java/com/cafe/study/domain/Apple.java
@@ -2,9 +2,11 @@ package com.cafe.study.domain;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
+import java.lang.reflect.Field;
 
 public class Apple {
 	private int price;
+	public int Name ;
 
     public int getPrice() {
         return price;
@@ -17,19 +19,35 @@ public class Apple {
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 
-		Apple apple = new Apple();
-        apple.setPrice(5);
-        System.out.println("Apple Price:" + apple.getPrice());
-        
-        String pak="com.cafe.study.domain.";
+//		Apple apple = new Apple();
+//        apple.setPrice(5);
+//        System.out.println("Apple Price:" + apple.getPrice());
+//        
+//        String pak="com.cafe.study.domain.";
 		
-        Class clz = Class.forName(pak+"Apple");
-        Method setPriceMethod = clz.getMethod("setPrice", int.class);
-        Constructor appleConstructor = clz.getConstructor();
-        Object appleObj = appleConstructor.newInstance();
-        setPriceMethod.invoke(appleObj, 14);
-        Method getPriceMethod = clz.getMethod("getPrice");
-        System.out.println("Apple Price:" + getPriceMethod.invoke(appleObj));
+//        Class clz = Class.forName(pak+"Apple");
+//        Method setPriceMethod = clz.getMethod("setPrice", int.class);
+//        Constructor appleConstructor = clz.getConstructor();
+//        Object appleObj = appleConstructor.newInstance();
+//        setPriceMethod.invoke(appleObj, 14);
+//        Method getPriceMethod = clz.getMethod("getPrice");
+//        System.out.println("Apple Price:" + getPriceMethod.invoke(appleObj));
+        
+//        Class clz = Apple.class;
+//        Apple apple = (Apple)clz.newInstance();
+//		Class clz = Apple.class;
+//		Constructor constructor = clz.getConstructor(String.class, int.class);
+//		Apple apple = (Apple)constructor.newInstance("红富士", 15);
+//      Method getPriceMethod = clz.getMethod("getPrice");
+//
+//		System.out.println(getPriceMethod.invoke(apple));
+		
+		Class clz = Apple.class;
+		Field[] fields = clz.getDeclaredFields();
+		for (Field field : fields) {
+		    System.out.println(field.getName());
+		}
+        
 		
 	}
 
---
+++ src/main/java/com/cafe/study/domain/Son.java
@@ -0,0 +1,5 @@
+package com.cafe.study.domain;
+
+public class Son {
+
+}
